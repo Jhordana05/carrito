29967ac647bd664c5cf84b465bd3aaaffc44a0a7
Jhordana Tania Condori Condori <jhorda05yoonji@gmail.com>
2026-09-16 19:23:41 -0400

    segunda parte: index

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
-from flask import Flask,render_template,request,session,redirect,url_for
+
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
-#necesario cuando se utiliza session
+
+# Necesario cuando se utiliza session
 app.secret_key = 'unaclavesecreta'
 
+
 @app.route("/")
-def carrito ():
+def carrito():
     if 'lista' not in session:
         session['lista'] = []
-        
-    return render_template('index.html',lista = session['lista'])
 
-    
-@app.route("/proceso",methods=['GET','POST'])
-def procesa ():
+    return render_template('index.html', lista=session['lista'])
+
+
+@app.route("/procesa", methods=['GET', 'POST'])
+def procesa():
     producto = request.form.get("producto")
+
     if 'lista' in session and producto:
         session['lista'].append(producto)
         session.modified = True
+
     return redirect(url_for("carrito"))
-    
-@app.route ("/vaciar",methods =["GET"])
+
+
+@app.route("/vaciar", methods=["GET"])
 def vaciar():
-    session.pop("lista",None)
+    session.pop("lista", None)
+
     return redirect(url_for("carrito"))
 
 
-    
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
