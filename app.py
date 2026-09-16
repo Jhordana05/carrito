820ce6bafbc5c4984558974dda5a99167f14b292
Jhordana Tania Condori Condori <jhorda05yoonji@gmail.com>
2026-09-15 23:51:02 -0400

    Parte 1: estructura inicial

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template,request,session,redirect,url_for
+
+app = Flask(__name__)
+#necesario cuando se utiliza session
+app.secret_key = 'unaclavesecreta'
+
+@app.route("/")
+def carrito ():
+    if 'lista' not in session:
+        session['lista'] = []
+        
+    return render_template('index.html',lista = session['lista'])
+
+    
+    
+if __name__ =="__main__":
+    app.run(debug=True)
